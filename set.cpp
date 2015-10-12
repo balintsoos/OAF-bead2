c539b88cb317a97351e814db68452e48c0500f5b
Balint Soos <soosbalint95@gmail.com>
2015-10-13 01:22:22 +0200

    seg fault bug FIXED

--- set.cpp
+++ set.cpp
@@ -15,11 +15,11 @@ using namespace std;
 // inicializalasnal a halmaz elemeit tartalmazo lancolt lista egy nullpointer
 Set::Set()
 {
-	root = 0;
+	root = new Node();
 }
 
 // constructor w/ argumentum
-// az argumentumkent atadott erteket berakja a halmazba
+// inicializal, majd az argumentumkent atadott erteket berakja a lancolt listaba
 Set::Set(int n)
 {
 	Set();
@@ -51,11 +51,12 @@ Set::Set(Set& s)
 	}
 	
 	// lista felepitese
-	root = new Node(s.root->value);
-	Node *thisCurrent = root;
-	Node *sCurrent = s.root;
+	root = new Node();
+	Node *thisCurrent = root->next;
+	Node *sCurrent = s.root->next;
 	while(sCurrent != 0)
 	{
+		thisCurrent = new Node(sCurrent->value);
 		thisCurrent->next = new Node(sCurrent->next->value);
 		thisCurrent = thisCurrent->next;
 		sCurrent = sCurrent->next;
@@ -76,11 +77,12 @@ Set& Set::operator= (Set& s)
 	}
 	
 	// lista felepitese
-	root = new Node(s.root->value);
-	Node *thisCurrent = root;
-	Node *sCurrent = s.root;
+	root = new Node();
+	Node *thisCurrent = root->next;
+	Node *sCurrent = s.root->next;
 	while(sCurrent != 0)
 	{
+		thisCurrent = new Node(sCurrent->value);
 		thisCurrent->next = new Node(sCurrent->next->value);
 		thisCurrent = thisCurrent->next;
 		sCurrent = sCurrent->next;
@@ -89,18 +91,14 @@ Set& Set::operator= (Set& s)
 }
 
 // elem berakasa a halmazba
-// ha a dinamikus tombunk ures, 0. indexre berakjuk ay elemet
-// ha merete nagyobb mint 0 es az adott elem nincs benne a halmazba
-// akkor letrehozunk egy eggyel hosszabb ideiglenes tombot, majd
-// a beszurasos rendezes algoritmusat alkalmazva atmasoljuk az elemeket
-// majd az ideiglenesbol atmasoljuk az eredeti tombbe
+// 
 // bemeneti adat: egesz szam, berakni kivant ertek
 // kimeneti adat: nincs (void)
 void Set::put(int n)
 {
 	if(isEmpty())
 	{
-		root = new Node(n);
+		root->next = new Node(n);
 	}
 	else
 	{
@@ -125,10 +123,7 @@ void Set::put(int n)
 }
 
 // elem kivetele a halmazbol
-// ha az adott elem benne van a tombben, akkor letrehozunk
-// egy eggyel rovidebb ideiglenes tombot es az adott elem
-// kivetelevel minden elemet atmasolunk, majd az ideiglenes
-// tombot atmasoljuk az eredetibe
+// 
 // bemeneti adat: egesz szam, amit ki szeretnenk venni a halmazbol
 // kimeneti adat: nincs (void)
 void Set::remove(int n)
@@ -154,13 +149,12 @@ void Set::remove(int n)
 }
 
 // halmaz megjelenitese
-// tombon vegighaladva kiirjuk a konzolra az elemeket
-// es a tomb meretet
+// listan vegighaladva kiirjuk a konzolra az elemeket
 // bemeneti ertek: nincs, kimeneti ertek: nincs
 void Set::print()
 {
-	cout << "Items: ";
-	Node *p = root;
+	cout << endl << "Items: ";
+	Node *p = root->next;
 
 	while(p != 0)
 	{
@@ -174,46 +168,46 @@ void Set::print()
 // ha egyik halmaz sem ures,
 // a halmazokon vegigiteralva a mindkettoben 
 // elofordulo elemeket kiirjuk
-// kihasznaljuk hogy a tomb rendezett:
-// hatulrol haladva parhuzamosan a tomb elemein, ha
+// kihasznaljuk hogy a lista rendezett:
+// haladva parhuzamosan a lista elemein, ha
 // a ket ertek megegyezik akkor metszetelemet talatunk
 // ha az ertek kulonbozik, akkor a relacio alapjan
-// az egyik tombon tovabbiteralunk
+// az egyik listan tovabbiteralunk
 // a folyamat addig folytatodik amig el nem erjuk 
-// valamelyik tomb veget
+// valamelyik lista veget
 // bemeneti adat: egy halmaz
 // kimeneti adat: nincs (void)
 void Set::intersection(Set& s)
 {
-// 	if (!isEmpty() && !s.isEmpty())
-// 	{
-// 		int iOfThis = size-1;
-// 		int iOfArg = s.getSize()-1;
-// 		int * itemsOfArg = s.getItems();
+	if (!isEmpty() && !s.isEmpty())
+	{
+		// int iOfThis = size-1;
+		// int iOfArg = s.getSize()-1;
+		// int * itemsOfArg = s.getItems();
 
-// 		while (iOfThis >= 0 && iOfArg >= 0)
-// 		{
-// 			if (items[iOfThis] > itemsOfArg[iOfArg])
-// 			{
-// 				iOfThis--;
-// 			}
-// 			else if (items[iOfThis] < itemsOfArg[iOfArg])
-// 			{
-// 				iOfArg--;
-// 			}
-// 			else if (items[iOfThis] == itemsOfArg[iOfArg])
-// 			{
-// 				cout << items[iOfThis] << " ";
-// 				iOfThis--;
-// 				iOfArg--;
-// 			}
-// 		}
-// 		cout << endl;
-// 	}
-// 	else
-// 	{
-// 		throw EMPTY;
-// 	}
+		// while (iOfThis >= 0 && iOfArg >= 0)
+		// {
+		// 	if (items[iOfThis] > itemsOfArg[iOfArg])
+		// 	{
+		// 		iOfThis--;
+		// 	}
+		// 	else if (items[iOfThis] < itemsOfArg[iOfArg])
+		// 	{
+		// 		iOfArg--;
+		// 	}
+		// 	else if (items[iOfThis] == itemsOfArg[iOfArg])
+		// 	{
+		// 		cout << items[iOfThis] << " ";
+		// 		iOfThis--;
+		// 		iOfArg--;
+		// 	}
+		// }
+		cout << endl;
+	}
+	else
+	{
+		throw EMPTY;
+	}
 }
 
 void Set::symDef(Set& s)
@@ -221,14 +215,14 @@ void Set::symDef(Set& s)
 	//
 }
 
-// tartalmaz-e a halmaz egz adott elemet
-// lineraris keresest alkalmazva keressuk az elemet a tombben
+// tartalmaz-e a halmaz egy adott elemet
+// lineraris keresest alkalmazva keressuk az elemet a listaban
 // bemeneti adat: egesz szam
 // kimeneti adat: logikai (bool)
 bool Set::isContain(int n)
 {
 	bool found = false;
-	Node *p = root;
+	Node *p = root->next;
 	while ((p != 0) && !found)
 	{
 		if(p->value == n)
@@ -245,5 +239,5 @@ bool Set::isContain(int n)
 // kimeneti adat: logikai (bool)
 bool Set::isEmpty()
 {
-	return root == 0;
+	return root->next == 0;
 }
